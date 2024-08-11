a11eccb72c35d7581cd1d230a18f72729a6d2fb5
moolenbeek <emoolenbeek@gmail.com>
2024-08-11 18:08:57 -0400

    add about and contact routes with svelte tabs

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -11,6 +11,6 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
   <div class="space-y-5">
-    <h1>this is the home page.</h1>
+    <h1>this is the contact page.</h1>
   </div>
 </div>
